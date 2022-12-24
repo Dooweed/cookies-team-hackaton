c3be2e9d076c3ee6a3bec87ef6f1d050e20a00c8
Maftuna <maftuna.ziyamova@midge-medical.com>
2022-12-24 20:23:02 +0500

    upd in gitignore

--- text_to_speech/utils.py
+++ text_to_speech/utils.py
@@ -1,13 +1,23 @@
+import os
+
 import azure.cognitiveservices.speech as speechsdk
+from azure.cognitiveservices.speech import AudioDataStream
+from dotenv import load_dotenv
 
-from text_to_speech.config import audio_config, speech_config
 
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+load_dotenv()
+# from text_to_speech.config import speech_config
+speech_config = speechsdk.SpeechConfig(subscription=os.environ['SPEECH_RECOGNITION_KEY'], region='eastus')
+
+# The language of the voice that speaks.
+speech_config.speech_synthesis_voice_name = 'uz-UZ-SardorNeural'  # uz-UZ-MadinaNeural
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
 
 
 def convert_to_audio(text: str) -> None:
     speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
-
+    stream = AudioDataStream(speech_synthesis_result)
+    print('stream hehe', stream)
     if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
         print("Speech synthesized for text [{}]".format(text))
     elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
@@ -17,3 +27,6 @@ def convert_to_audio(text: str) -> None:
             if cancellation_details.error_details:
                 print("Error details: {}".format(cancellation_details.error_details))
                 print("Did you set the speech resource key and region values?")
+
+
+convert_to_audio('Bugun turdim')
