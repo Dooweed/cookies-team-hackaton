c316e07f233359c2cb6bf1ce75ad758d9ad7bf11
David <bydev2001@gmail.com>
2022-12-24 21:06:47 +0500

    Initial commit

---
+++ website/js/youtube.js
@@ -0,0 +1,29 @@
+var player;
+
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+let done = false;
+
+function loadYoutubePlayer(videoId) {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: videoId,
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
