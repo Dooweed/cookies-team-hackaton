e1465b0331788230db8a6b551a1bf57e95963acb
David <bydev2001@gmail.com>
2022-12-25 14:33:12 +0500

    Updated website

--- website/js/page.js
+++ website/js/page.js
@@ -1,3 +1,6 @@
-
-
-document.addEventListener('')
+// const tryItBtn = document.querySelector('#main-try-it'),
+//     characterImg = document.querySelector('#character');
+//
+// tryItBtn.addEventListener('mouseover', (e) => {
+//     characterImg.setAttribute('src', 'images/character-2.png');
+// })
